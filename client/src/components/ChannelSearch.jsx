c0ae8d03c6be13875e28ce35a34dd318258c01bc
heniLtaief <heniltaief@gmail.com>
2021-12-18 22:41:14 +0100

    setting input

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -3,13 +3,26 @@ import { useChatContext } from 'stream-chat-react';
 import {SearchIcon} from "../assets";
 
 const ChannelSearch = () => {
+    const [query, setQuery] = useState('');
+    const [loading,setLoading] = useState(false);
+
+    const onSearch = (event)=>{
+        event.preventDefault();
+        setLoading(true);
+        setQuery(event.target.value);
+    }
     return (
         <div className='channel-search__container'>
           <div className='channel-search__input__wrapper'>
               <div className='channel-search__input__icon'>
                 <SearchIcon/>
               </div>
-              <input className='channel-search__input__text'/>
+              <input className='channel-search__input__text' 
+              placeholder='search' 
+              type="text" 
+              value={query}
+              onChange={onSearch}
+              />
           </div>  
         </div>
     )
