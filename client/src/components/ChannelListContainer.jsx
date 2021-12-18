cf2bc6b5d48be5d98fd4466225a8d4ddb0a11073
heniLtaief <heniltaief@gmail.com>
2021-12-18 22:11:18 +0100

    creating sidebar

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { ChannelList, useChatContext} from "stream-chat-react";
-import { ChannelSearch, TeamChannelList, TeamChannelPreview} from "stream-chat-react";
+import { ChannelSearch, TeamChannelList, TeamChannelPreview} from "./";
+import Cookies from 'universal-cookie';
 
+const SideBar = ()=>(
+    <div className='channel-list__sidebar'>
+        <div className='channel-list__sidebar__icon1'>
+            <div className='icon1__inner'>
+                <img src={HospitalIcon}/>
+            </div>
+        </div>
+    </div>
+)
 const ChannelListContainer = () => {
     return (
         <div>
