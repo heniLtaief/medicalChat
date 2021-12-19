cc84e817fecc621904ae36bb450b995454731290
heniLtaief <heniltaief@gmail.com>
2021-12-19 12:10:16 +0100

    rendering the teamchannel messages

--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -11,9 +11,22 @@ const TeamChannelList = ({children, error=false, loading, type}) => {
             </div>
         ) : null
     }
+    if (loading){
+        return (
+            <div className="team-channel-list">
+                <p className='team-channel-list__message loading'>
+                    {type === "team" ? "Channels" : "Messages"} loading...
+                </p>
+            </div>
+        )
+    }
     return (
-        <div>
-            
+        <div className='team-channel-list'>
+         <div className='team-channel-list__header'>
+             <p className='team-channel-list__header__title'>
+                 {type === "team" ? "Channels" : "Direct Messages"}
+             </p>
+         </div>   
         </div>
     )
 }
