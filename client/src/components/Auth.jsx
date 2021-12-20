f8a11327c7d0ed6fba1f929b167b36b3a9e7fd81
heniLtaief <heniltaief@gmail.com>
2021-12-20 22:19:00 +0100

    setting input

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -5,12 +5,26 @@ import signinImage from "../assets/signup.jpg";
 
 const Auth = () => {
     const [isSignup,setIsSignup] = useState(false);
-
+    const handleChange = ()=> {}
     return (
         <div className='auth__form-container'>
             <div className='auth__form-container_fields'>
                 <div className='auth__form-container_fields-content'>
                     <p>{isSignup ? "Sign Up" : "Sign In"}</p>
+                    <form onSubmit={()=>{}}>
+                        {isSignup && (
+                            <div className='auth__form-container_fields-content_input'>
+                                <label htmlFor='fullName'>Full Name</label>
+                                <input 
+                                    name= "fullName"
+                                    type= "text"
+                                    placeholder='Full Name'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                    </form>
                 </div>
             </div>
         </div>
