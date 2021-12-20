7e21cacacdeb1a4a604d6702ea55f768257206f4
heniLtaief <heniltaief@gmail.com>
2021-12-20 22:30:43 +0100

    signup inputs

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import signinImage from "../assets/signup.jpg";
 
 const Auth = () => {
-    const [isSignup,setIsSignup] = useState(false);
+    const [isSignup,setIsSignup] = useState(true);
     const handleChange = ()=> {}
     return (
         <div className='auth__form-container'>
@@ -24,6 +24,50 @@ const Auth = () => {
                                 />
                             </div>
                         )}
+                         <div className='auth__form-container_fields-content_input'>
+                                <label htmlFor='username'>username</label>
+                                <input 
+                                    name= "username"
+                                    type= "text"
+                                    placeholder='username'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            {isSignup && (
+                            <div className='auth__form-container_fields-content_input'>
+                                <label htmlFor='PhoneNumber'>Phone Number</label>
+                                <input 
+                                    name= "PhoneNumber"
+                                    type= "text"
+                                    placeholder='Phone Number'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        {isSignup && (
+                            <div className='auth__form-container_fields-content_input'>
+                                <label htmlFor='avatarURL'>Avatar URL</label>
+                                <input 
+                                    name= "AvatarURL"
+                                    type= "text"
+                                    placeholder='Avatar URL'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className='auth__form-container_fields-content_input'>
+                                <label htmlFor='password'>Password</label>
+                                <input 
+                                    name= "password"
+                                    type= "password"
+                                    placeholder='Password'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
                     </form>
                 </div>
             </div>
