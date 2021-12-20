93262db4b992615c915e29a2f831b48d4a5abb91
heniLtaief <heniltaief@gmail.com>
2021-12-20 22:11:53 +0100

    setting signin signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,16 @@ import {StreamChat} from "stream-chat";
 import {Chat} from "stream-chat-react";
 import Cookies from "universal-cookie";
 
-import {ChannelContainer, ChannelListContainer} from "./components";
+import {ChannelContainer, ChannelListContainer, Auth} from "./components";
 import "./App.css";
 
 const apiKey = "yq7qqb6re2xx";
 const client = StreamChat.getInstance(apiKey);
+const authToken = false;
 
 const App = () => {
+    if (!authToken) return <Auth/>
+    
     return (
         <div className='app__wrapper'>
             <Chat client={client} theme="team light">
