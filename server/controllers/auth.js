bd539d00b794646254d7a16d97dada89b09b6afe
heniLtaief <heniltaief@gmail.com>
2021-12-21 15:52:24 +0100

    adding environemnt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,10 +3,18 @@ const bcrypt = require ("bcrypt");
 const streamChat= require("stream-chat");
 const crypto = require("crypto");
 
+const api_key= process.env.STREAM_API_KEY;
+const api_secret= process.env.STREAM_API_SECRET;
+const app_id= process.env.STREAM_APP_ID;
+
+
+
 const login = (req,res)=>{
     try {
        const {fullName,username,phoneNumber,password}= req.body;
-       const userId= crypto 
+
+       const userId= crypto.randomBytes(16).toString("hex") // create a random sequence of hexadecimal digites
+       const serverClient= connect(api_key,api_secret,app_id)
     } catch (error) {
         console.log(error)
         res.status(500).json({message: error})
