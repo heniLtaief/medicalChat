464c2e92b74df4fd54035b265650176751416c8d
heniLtaief <heniltaief@gmail.com>
2021-12-21 15:06:24 +0100

    setting the routes & controllers

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express= require("express");
 const cors= require("cors");
-
+const authRoutes = require("./routes/auth.js")
 const app= express();
 PORT = process.env.PORT || 5000;
 
 // it allows us to call all the variable from .env inside of our node app
 require("dotenv").config();
+
+// setting our middleware
+app.use(cors()); // allows us to make cross origin requests
+app.use(express.json()); // allows us to pass json payloads from the backend to the frontend
+app.use(express.urlencoded());
+
+app.get("/", (req,res)=>{
+    res.send("Hello!")
+});
+
+app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`));
