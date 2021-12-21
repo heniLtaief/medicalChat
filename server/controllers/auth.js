e723e18f12db6213c16848b14615405e89ee0c94
heniLtaief <heniltaief@gmail.com>
2021-12-21 15:35:58 +0100

    setting login signup fn

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,18 @@
-const login = ()=>{};
-const signup = ()=>{};
+const login = (req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: error})
+    }
+};
+const signup = ()=>{
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: error})
+    }
+};
 
 module.exports={signup,login}
