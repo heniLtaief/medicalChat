456139f6314be47785617040a36e25d7f312a43b
heniLtaief <heniltaief@gmail.com>
2021-12-21 15:59:51 +0100

    finish signup work on login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -9,20 +9,32 @@ const app_id= process.env.STREAM_APP_ID;
 
 
 
-const login = (req,res)=>{
+const signup = async (req,res)=>{
     try {
        const {fullName,username,phoneNumber,password}= req.body;
 
        const userId= crypto.randomBytes(16).toString("hex") // create a random sequence of hexadecimal digites
-       const serverClient= connect(api_key,api_secret,app_id)
+
+       const serverClient= connect(api_key,api_secret,app_id) // we will use this to create a new user Token
+
+       const hashedPassword= await bcrypt.hash(password, 10);
+
+       const token= serverClient.createUserToken(userId);
+
+       res.status(200).json({token,fullName,username,phoneNumber,userId, hashedPassword})
+
     } catch (error) {
         console.log(error)
         res.status(500).json({message: error})
     }
 };
-const signup = ()=>{
+const login = async ()=>{
     try {
-        
+        const {username, password}= req.body;
+
+       const serverClient= connect(api_key,api_secret,app_id);
+       const client= streamChat.getInstance()
+
     } catch (error) {
         console.log(error)
         res.status(500).json({message: error})
