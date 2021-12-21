a9d99022edb4b8c2201a28968fce3126ed2f27c9
heniLtaief <heniltaief@gmail.com>
2021-12-21 15:40:38 +0100

    importing all the packages

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,12 @@
+const {connect} = require ("getstream");
+const bcrypt = require ("bcrypt");
+const streamChat= require("stream-chat");
+const crypto = require("crypto");
+
 const login = (req,res)=>{
     try {
-        
+       const {fullName,username,phoneNumber,password}= req.body;
+       const userId= crypto 
     } catch (error) {
         console.log(error)
         res.status(500).json({message: error})
